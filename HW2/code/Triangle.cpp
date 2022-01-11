fb1317bfd141fdc579ab2352e1b380c60d55b969
Erce Güder <erce_guder@icloud.com>
2022-01-11 13:14:32 +0300

    convert to getLines()

--- HW2/code/Triangle.cpp
+++ HW2/code/Triangle.cpp
@@ -6,10 +6,13 @@ Triangle::Triangle(){
     this->vertexIds[2] = -1;
 }
 
-Triangle::Triangle(int vid1, int vid2, int vid3){
+Triangle::Triangle(int vid1, int vid2, int vid3, vector<Vec3*> vertices){
     this->vertexIds[0] = vid1;
     this->vertexIds[1] = vid2;
     this->vertexIds[2] = vid3;
+
+    for (int i=0; i < 3; i++)
+        this->vertices[i] = Vec4(*(vertices[this->vertexIds[i] - 1]), 1);
 }
 
 Triangle::Triangle(const Triangle &other){
@@ -18,11 +21,7 @@ Triangle::Triangle(const Triangle &other){
     this->vertexIds[2] = other.vertexIds[2];
 }
 
-void Triangle::setVariables(vector<Vec3*> vertices){
-
-    for (int i=0; i < 3; i++)
-        this->vertices[i] = Vec4(*(vertices[this->vertexIds[i] - 1]), 1);
-
+void Triangle::setLines(){
     for (int i=0; i < 3; i++)
         this->lines[i] = Line(this->vertices[i], this->vertices[(i+1) % 3]);
 }
