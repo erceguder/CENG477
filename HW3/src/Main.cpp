64b1c31bf051890f2df91d4925e5cd2203a833ad
Erce Güder <erce_guder@icloud.com>
2022-02-05 21:49:27 +0300

    add HW3

---
+++ HW3/src/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "EclipseMap.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    EclipseMap *openGL = new EclipseMap();
+	//openGL->Render(argv[2],argv[1],argv[3]);
+    openGL->Render("earth_texture_map.jpg", "height_map.jpg", "moon_texture_map.jpg");
+}
