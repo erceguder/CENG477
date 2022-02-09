57507cd511a81c8005dff93b4f53983d675d096e
Erce Güder <erce_guder@icloud.com>
2022-02-09 21:40:22 +0300

    add argv

--- HW3/src/Main.cpp
+++ HW3/src/Main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     EclipseMap *openGL = new EclipseMap();
-	//openGL->Render(argv[2],argv[1],argv[3]);
-    openGL->Render("earth_texture_map.jpg", "height_map.jpg", "moon_texture_map.jpg");
+	openGL->Render(argv[2],argv[1],argv[3]);
+    // openGL->Render("earth_texture_map.jpg", "height_map.jpg", "moon_texture_map.jpg");
 }
